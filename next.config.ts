cbb376a49d553bf5fe7f85f8aeabfcf2804c7584
Saikou <khanhpronam@gmail.com>
2025-01-06 21:05:26 -0800

    Fixed bug

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */ //delete this if you want to run locally
   output: "export", 
-  basePath: "/kuankongy.github.io",
-  assetPrefix: "/kuankongy.github.io/",
   images: {
     unoptimized: true
   }
